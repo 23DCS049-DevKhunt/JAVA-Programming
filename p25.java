0eed6a07615a69fb2129a9954a0b75015b6df581
23DCS049-DevKhunt <23dcs049@charusat.edu.in>
2024-10-15 16:49:33 +0530

    Create p25.java

---
+++ p25.java
@@ -0,0 +1,11 @@
+public class p25 {
+    public static void main(String[] args) {
+    try {
+    throw new Exception("This is a custom exception");
+    } catch (Exception e) {
+    System.out.println("Caught an exception: " + e.getMessage());
+    }
+    System.out.print("\nName : Dev Khunt_23DCS049 ");
+    
+    }
+    }
